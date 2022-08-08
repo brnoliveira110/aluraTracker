19bbb53f1573033d5fcf06e50be90cb48a7c4410
Caio Santana <brnoliveirasantana@gmail.com>
2022-08-08 20:39:12 -0300

    06
    Cadastrando projetos

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,40 +1,58 @@
 <template>
   <header>
-      <h1>
-        <img src="../assets/logo.png" alt="">  
-      </h1>
+    <h1>
+      <img src="../assets/logo.png" alt="" />
+    </h1>
+    <div class="has-text-centered">
       <button class="button" @click="alterarTema">
         {{ textoBotao }}
       </button>
+    </div>
+    <nav class="panel mt-5">
+      <ul>
+        <li>
+          <router-link to="/" class="link">
+            <i class="fas fa-tasks"></i>
+            Tarefas
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/projetos" class="link">
+            <i class="fas fa-project-diagram"></i>
+            projetos
+          </router-link>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'BarraLateral',
-  emits: ['aoTemaAlterado'],
-  data () {
+  name: "BarraLateral",
+  emits: ["aoTemaAlterado"],
+  data() {
     return {
-      modoEscuroAtivo: false
-    }
+      modoEscuroAtivo: false,
+    };
   },
   computed: {
-    textoBotao () {
+    textoBotao() {
       if (this.modoEscuroAtivo) {
-        return 'Desativar modo escuro'
+        return "Desativar modo escuro";
       }
-      return 'Ativar modo escuro'
-    }
+      return "Ativar modo escuro";
+    },
   },
   methods: {
-    alterarTema () {
-      this.modoEscuroAtivo = !this.modoEscuroAtivo
-      this.$emit('aoTemaAlterado', this.modoEscuroAtivo)
-    }
-  }
-})
+    alterarTema() {
+      this.modoEscuroAtivo = !this.modoEscuroAtivo;
+      this.$emit("aoTemaAlterado", this.modoEscuroAtivo);
+    },
+  },
+});
 </script>
 
 <style scoped>
@@ -50,5 +68,18 @@ header {
     padding: 2.5rem;
     height: auto;
   }
+}
+
+.panel li {
+  margin: 8px 0;
+}
+.link {
+  color: #fff;
+}
+.link:hover {
+  color: #faf0ca;
+}
+.link.router-link-active {
+  color: #faf0ca;
 }
 </style>
---
+++ src/interfaces/IProjeto.ts
@@ -0,0 +1,5 @@
+export default interface Iprojeto{
+    id: string;
+    nome: string;
+
+}
